af08b76dd8c432d873e5a608c0e2ad13fc4c777c
shradha <shradhaparkgi123@gmail.com>
2020-05-19 23:44:25 +0530

    Updates

---
+++ precache-manifest.8aa24daffd8d1318b284dfab2b6a8f98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6aeecc6a4498c58dee49c803760755f",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "62bb27ced143f24115e6",
+    "url": "/covid-19/static/css/main.6805c9b3.chunk.css"
+  },
+  {
+    "revision": "d595daf857ddfbe4d492",
+    "url": "/covid-19/static/js/2.369c1053.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.369c1053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bb27ced143f24115e6",
+    "url": "/covid-19/static/js/main.ae5e0a1b.chunk.js"
+  },
+  {
+    "revision": "c3a0e7d430a393e4ef8d",
+    "url": "/covid-19/static/js/runtime-main.3aee19ac.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
